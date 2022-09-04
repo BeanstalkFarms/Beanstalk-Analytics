7ec2e56ea195b829dc33e2a6aa00b63396b4fa56
Bryce Morrow <brycemorrow4564@gmail.com>
2022-09-03 20:34:26 -0400

    Added single handler function

---
+++ api/index.py
@@ -0,0 +1,19 @@
+from urllib.parse import urlparse, parse_qs
+from pathlib import Path 
+from http.server import BaseHTTPRequestHandler
+
+
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'application/json')
+    self.end_headers()
+    parsed_url = urlparse(self.path)
+    qs = parse_qs(parsed_url.query)
+    assert len(qs['name']) == 1
+    name = qs['name'][0]
+    with Path(f"schemas/{name}.json").open("r") as f: 
+        json_str = f.read()
+    self.wfile.write(json_str.encode(encoding='utf_8'))
+    return
