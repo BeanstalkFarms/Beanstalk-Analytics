cca8d679512360c5933880a8542b967d93d12bc0
Cool Bean <anoncoolbean@gmail.com>
2022-10-21 13:49:37 -0500

    fix: style footer for mobile

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -5,12 +5,24 @@ const Footer: React.FC<{
 }> = (props) => {
   return (
     <div className="flex flex-row justify-center gap-10 pt-2 pb-7">
-      <a href="https://docs.bean.money/">Docs</a>
-      <a href="https://discord.gg/beanstalk">Discord</a>
-      <a href="https://twitter.com/beanstalkfarms">Twitter</a>
-      <a href="https://immunefi.com/bounty/beanstalk">Bug Bounty</a>
-      <a href="https://github.com/beanstalkfarms">GitHub</a>
-      <a href="https://docs.bean.money/disclosures">Disclosures</a>
+      <a href="https://docs.bean.money/" target="_blank" rel="noreferrer">
+        Docs
+      </a>
+      <a href="https://discord.gg/beanstalk" target="_blank" rel="noreferrer">
+        Discord
+      </a>
+      <a href="https://twitter.com/beanstalkfarms" target="_blank" rel="noreferrer">
+        Twitter
+      </a>
+      <a href="https://immunefi.com/bounty/beanstalk" target="_blank" rel="noreferrer">
+        Bug Bounty
+      </a>
+      <a href="https://github.com/beanstalkfarms" target="_blank" rel="noreferrer" className="md:inline-block hidden">
+        GitHub
+      </a>
+      <a href="https://docs.bean.money/disclosures" target="_blank" rel="noreferrer" className="md:inline-block hidden">
+        Disclosures
+      </a>
     </div>
   )
 }
